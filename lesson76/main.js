81ef02ac4db7bee0a9985a86b7be7c3fd47f6645
mamontovv <75908460+mamontovv@users.noreply.github.com>
2024-10-06 16:05:32 +0300

    Add files via upload

---
+++ lesson76/main.js
@@ -0,0 +1,13 @@
+const firstName = 'Alexey'
+
+const surname = 'Mamontov'
+
+const lastName = 'Denisovich'
+
+let age = 17
+
+let city = 'Saint Petersburg'
+
+console.log(firstName, surname, lastName)
+console.log(age, 'y.o.')
+console.log(city)

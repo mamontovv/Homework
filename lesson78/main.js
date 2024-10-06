a1fea8c6b9d4d84eae4daf09ebc226ef7a74b5fb
mamontovv <75908460+mamontovv@users.noreply.github.com>
2024-10-06 16:33:08 +0300

    Add files via upload

---
+++ lesson78/main.js
@@ -0,0 +1,12 @@
+const age = 18
+
+const checkAge = 18
+
+if ( age >= checkAge) {
+    const result = 'Вы совершеннолетний'
+    console.log(result)
+}
+else {
+    const result = 'Вам еще нет 18'
+    console.log(result)
+}

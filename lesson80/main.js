b97e38890a83c84983fdb63773f6a6c6051bd6b1
mamontovv <75908460+mamontovv@users.noreply.github.com>
2024-10-08 20:44:16 +0300

    Add files via upload

---
+++ lesson80/main.js
@@ -0,0 +1,22 @@
+for (let a = 2; a<13; a++) {
+    if (a % 2 == 0) {
+        console.log(a)
+    }
+}
+
+for (let b = 10; b != 0; b=b-1) {
+    console.log(b)
+}
+
+let c = 0
+let d = 11
+
+while(c < 12) {
+    c = c + 2
+    console.log(c)
+} 
+
+while ( d != 1) {
+    d--
+    console.log(d)
+}
---
+++ lesson81/main.js
@@ -0,0 +1,13 @@
+const numbers = [1, 2, 3, 4, 5, 6]
+
+for(let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 == 0) {
+        console.log(numbers[i])
+    }
+}
+
+const rainbow = ['red', 'orange', 'yellow', 'green', 'light blue', 'blue', 'purple']
+
+for(let a = 6; a != -1; a--) {
+    console.log(rainbow[a])
+}

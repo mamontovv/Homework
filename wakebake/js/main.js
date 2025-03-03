d0812b1f6daadd9e8e527f0b3f19bea52570ae18
mamontovv <75908460+mamontovv@users.noreply.github.com>
2025-03-03 21:25:24 +0300

    Update main.js

--- wakebake/js/main.js
+++ wakebake/js/main.js
@@ -270,3 +270,7 @@ const swiper2 = new Swiper('.feedback__swiper', {
         }
     }
   })
+
+  const telInputs = document.querySelectorAll('input[type="tel"]')
+  const im = new Inputmask('+7 (999) 999-99-99')
+  im.mask(telInputs)
